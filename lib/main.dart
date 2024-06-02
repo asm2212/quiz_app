df57aa95c6039dd2c90e64faff3dcc74c678403e
asm2212 <asmareadmasu0@gmail.com>
2024-06-02 17:03:31 +0300

    animate

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+     home: HomePage()
     );
   }
 }
 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
